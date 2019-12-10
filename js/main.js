984a974157b59faf5a6ffdc2093a98eae74153f7
Svetlana Solodovnikova <svslight@yandex.ru>
2019-12-10 13:55:04 +0500

    Выполнен - Footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    /* Портфолио */
+$(document).ready(function () {
+   /* Портфолио */
    $('.button[filter]').click(function () {
       if ($(this).attr('val') == 'off') {
          $('.button[filter]').attr('val', 'off').removeClass('focused');
@@ -13,26 +13,27 @@ $(document).ready(function() {
          }
       }
    });
-//    $('.button[filter]').click(function(){
-//        if($(this).attr('filter')=='all') {
-//            if($(this).attr('val')=='off') {
-//                $('.button[filter]').attr('val', 'off');
-//                $(this).attr('val', 'on');  
-//                $('.button[filter]').removeClass('focused');
-//                $(this).addClass('focused');
-//                $('.filter > div').show(300);
-//            }
-//        } else
-//        if($(this).attr('val')=='off') {
-//            $('.button[filter]').attr('val', 'off');
-//            $(this).attr('val', 'on');   
-//            $('.button[filter]').removeClass('focused');
-//            $(this).addClass('focused');
-//            $('.filter > div').hide(300);
-//            var filter = $(this).attr('filter');
-//            $('.filter > div[filter='+filter+']').show(300);
-//        }            
-//    });  
-    
-    
+   /* Слайдер */
+   $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      dots: true,
+      dotsClass: 'dots-style',
+      responsive: [{
+         breakpoint: 1025,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+         }
+      }, {
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+         }
+      }]
+    });
+     
 });
