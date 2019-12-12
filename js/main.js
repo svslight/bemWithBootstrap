95720ad92198d7427e9fddbf0420c8b4ca690483
Svetlana Solodovnikova <svslight@yandex.ru>
2019-12-12 11:27:19 +0500

    Адаптивная версия для Планшетов-iPad

--- js/main.js
+++ js/main.js
@@ -22,18 +22,59 @@ $(document).ready(function () {
       dots: true,
       dotsClass: 'dots-style',
       responsive: [{
-         breakpoint: 1025,
+         breakpoint: 1024,
          settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
          }
-      }, {
-         breakpoint: 480,
-         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-         }
       }]
     });
-     
+
+    /*Меню - переход по ссылкам*/ 
+   $('ul.menu a[href^="#"').click(function(){
+      var target = $(this).attr('href');
+      $('html, body').animate({
+         scrollTop: $(target).offset().top
+      }, 500);
+      $('ul.menu a[href^="#"').css({
+         'color': '#212121'
+      });
+      $(this).css({
+         'color': '#004bee'
+      });
+      return false;
+   });
+
+   /* Выпадающее меню */
+   $('.menu-icon').click(function () {
+      $('nav').slideToggle(500);
+      $('ul.menu').css({
+         'display': 'flex',
+         'flex-direction': 'column'
+      });
+      if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
+         $(this).html('<i class="fas fa-times"></i>');
+      } else {
+         $(this).html('<i class="fas fa-bars"></i>');
+      }
+   });
+
+   /* Кнопка наверх */  
+   $(window).scroll(function () {
+      if ($(this).scrollTop() != 0)
+         $('#toTop').fadeIn();
+      else
+         $('#toTop').fadeOut();
+   });
+   $('#toTop').click(function () {
+      $('body,html').animate({
+         scrollTop: 0
+      }, 800);
+   });
+
 });
+
+// var btn = document.querySelectorAll('button');
+// for (var i = 0; i < btn.length; i++) {
+//    btn[i].style.outline = 'none';
+// }
